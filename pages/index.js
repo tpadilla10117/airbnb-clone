a083ebfa1be80e68bdde77339a91af958a8bb500
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 14:37:15 -0700

    fetched data from papareact.com

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'; //Component from Next.js
 
 import { Header, Banner } from './utils';
 
-export default function Home() {
+export default function Home( { exploreData }) {
   return (
     <div className="">
       <Head>
@@ -15,11 +15,37 @@ export default function Home() {
       {/* Header will go here */}
       <Header />
 
-
-
       {/* Banner will go here */}
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endpoints from Sonny Sangha */}
+          {exploreData?.map( (item) => (
+            <h1>{ item.location }</h1>
+          ) )}
+
+        </section>
+      </main>
       
     </div>
-  )
+  );
 }
+
+/* Initiating some static rendering; fetch data from papareact.com: */
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+  .then(res => res.json() 
+
+  );
+
+  return {
+    //Can return the data as props:
+    props: {
+      exploreData
+    }
+  }
+};
