6d769ca9df968a4d2a7878a2c57c75df56b0aabe
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 15:05:30 -0700

    finished styling the SmallCard component

--- pages/index.js
+++ pages/index.js
@@ -23,16 +23,18 @@ export default function Home( { exploreData }) {
           
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
-          {/* Pull some data from a server - API endpoints from Sonny Sangha */}
-          {exploreData?.map( ({img, distance, location}) => (
-            <SmallCard 
-              key={ img }
-              img={ img }
-              distance={ distance }
-              location = { location }
-            
-            />
-          ) )}
+        {/* Pull some data from a server - API endpoints from Sonny Sangha */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map( ({img, distance, location}) => (
+              <SmallCard 
+                key={ img }
+                img={ img }
+                distance={ distance }
+                location = { location }
+              
+              />
+            ) )}
+          </div>
 
         </section>
       </main>
