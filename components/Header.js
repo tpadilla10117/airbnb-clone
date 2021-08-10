440b7004e14cd48b17d7a41487cefbbb76fb9871
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 13:37:42 -0700

    made a responsive searchbar in the Header.js component

--- components/Header.js
+++ components/Header.js
@@ -18,9 +18,10 @@
                 </div>
 
                 {/* Middle Section (Search bar) */}
-                <div>
-                    <input type='text' placeholder="Start your search" />
-                    <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer" />
+                <div className="flex items-center md:border-2 md:shadow-sm rounded-full py-2 ">
+                    <input className="flex-grow pl-5 bg-transparent outline-none" type='text' placeholder="Start your search" />
+
+                    <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex md:mx-2" />
                 </div>
 
                 {/* Right */}
