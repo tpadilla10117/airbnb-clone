600a2ef3f37f869e9f529e6042d2f1475a8fb4cd
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 13:51:58 -0700

    added right section to header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 /* Header functional component: */
 
     import Image from 'next/image'; //Next.js img tag
-    import { SearchIcon } from '@heroicons/react/solid'; //Importing icon from heroicons
+    import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon } from '@heroicons/react/solid'; //Importing icon from heroicons
 
     function Header() {
         return (
@@ -19,13 +19,24 @@
 
                 {/* Middle Section (Search bar) */}
                 <div className="flex items-center md:border-2 md:shadow-sm rounded-full py-2 ">
-                    <input className="flex-grow pl-5 bg-transparent outline-none" type='text' placeholder="Start your search" />
+                    <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type='text' placeholder="Start your search" />
 
                     <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex md:mx-2" />
                 </div>
 
-                {/* Right */}
-                <div>
+                {/* Right Section: */}
+                <div className="flex items-center space-x-4 justify-end text-gray-500" >
+
+                    <p className="hidden md:inline cursor-pointer">Become a host</p>
+
+                    <GlobeAltIcon className="h-6 cursor-pointer" />
+
+                    <div className="flex items-center space-x-2 border-2 p-2 rounded-full" >
+                        <MenuIcon className="h-6" />
+
+                        <UserCircleIcon className="h-6" />
+
+                    </div>
 
                 </div>
 
