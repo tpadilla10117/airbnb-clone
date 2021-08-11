541654e53c31c5de78208b3e8dc4bfd927530f62
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 17:42:01 -0700

    created a search.js component

--- pages/utils.js
+++ pages/utils.js
@@ -1,5 +1,7 @@
 /* Utitlity file to export components and pages: */
 
+import { fromJSON } from 'postcss';
+
 export { default as Header } from '../components/Header';
 
 export { default as Banner } from '../components/Banner';
@@ -11,3 +13,5 @@ export { default as MediumCard } from '../components/MediumCard';
 export { default as LargeCard } from '../components/LargeCard';
 
 export { default as Footer } from '../components/Footer';
+
+export { default as Search } from './search';
