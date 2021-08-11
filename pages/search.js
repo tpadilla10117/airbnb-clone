d323707c7ff6e7dbd05206186b54a5856e1ca089
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 19:26:28 -0700

    added functional components to my Search component

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,20 @@
 
+    import { Header, Footer } from './utils.js';
+
     function Search() {
         return (
             <div>
+                <Header />
 
+                    <main className="flex">
+
+                        <section>
+                            <p>300+ stays for 5 number of guests</p>
+                        </section>
+
+                    </main>
+
+                <Footer />
             </div>
         );
     };
