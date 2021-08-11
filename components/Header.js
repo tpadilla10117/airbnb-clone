b735becc4ad4f761acc281a027fd6a3d2776c3ea
Trinidad Padilla <tpadilla101@live.com>
2021-08-10 22:26:29 -0700

    fixed the placeholder when searching.  Can also grab query parameters in the url when choosing dates in the calendar

--- components/Header.js
+++ components/Header.js
@@ -6,8 +6,12 @@
     import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UsersIcon } from '@heroicons/react/solid'; //Importing icon from heroicons
 
     import { DateRangePicker } from 'react-date-range';
+    import { useRouter } from "next/dist/client/router";
 
-    function Header() {
+    function Header( {placeholder} ) {
+
+    /* Use the custom react router (hook) built into Next.js: */
+        const router = useRouter();
 
     /* State that Handles SearchInput values: */
         const [ searchInput, setSearchInput ] = useState("");
@@ -37,13 +41,26 @@
             setSearchInput('');
         };
 
+    /* This search function allows us to use query-parameters to add into url and retain search data:  */
+    /* Used for the search button in the calendar */
+        const search = () => {
+            router.push( {
+                pathname: '/search',
+                query: {
+                    location: searchInput,
+                    startDate: startDate.toISOString(),
+                    endDate: endDate.toISOString(),
+                    noOfGuests,
+                },
+            })
+        }
 
         return (
             <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md py-5 px-5 md:px-10"> 
                 
 
                 {/* Left-Hand side section: */}
-                <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+                <div className='relative flex items-center h-10 cursor-pointer my-auto' onClick={ () => router.push("/") }>
                     <Image src='https://links.papareact.com/qd3'
                         layout="fill"
                         objectFit="contain"/* Stops from changing aspect ratio */
@@ -53,7 +70,7 @@
 
                 {/* Middle Section (Search bar) */}
                 <div className="flex items-center md:border-2 md:shadow-sm rounded-full py-2 ">
-                    <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type='text' placeholder="Start your search" value={searchInput} onChange={ (event) => setSearchInput(event.target.value) } />
+                    <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type='text' placeholder={placeholder || "Start your search"} value={searchInput} onChange={ (event) => setSearchInput(event.target.value) } />
 
                     <SearchIcon className="h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex md:mx-2" />
                 </div>
@@ -103,7 +120,7 @@
                         <div className="flex">
 
                             <button className="flex-grow text-gray-500" onClick={resetInput}>Cancel</button>
-                            <button className="flex-grow text-red-400">Search</button>
+                            <button className="flex-grow text-red-400" onClick={search}>Search</button>
 
                         </div>
 
--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,34 @@
 
+    import { useRouter } from 'next/dist/client/router';
     import { Header, Footer } from './utils.js';
+    import { format } from "date-fns"; //format function from date-fns library
 
     function Search() {
+
+    /* Use ES6 destructuring to grab query data from search that we can use in the component: */
+        const router = useRouter();
+        const { location, startDate, endDate, noOfGuests } = router.query;
+
+    /* Use the format obj fromdate-fns to format the date objects in the UI: */
+        const formattedStartDate = format(new Date(startDate), "dd MMMM yy" );
+        const formattedEndDate = format(new Date(endDate), "dd MMMM yy" );
+    /* Pass ' range ' for the dates */
+        const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+        /* console.log(router.query) */
+
         return (
             <div>
-                <Header />
+                <Header placeholder={ `${location} | ${range} | ${noOfGuests} guests` } />
 
                     <main className="flex">
 
-                        <section>
-                            <p className="text-xs">300+ Stays for 5 number of guests</p>
+                        <section className="flex-grow pt-14 px-6">
+                            <p className="text-xs">300+ Stays - {range} - for { noOfGuests } guests</p>
 
-                            <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+                            <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in { location }</h1>
 
-                            <div className="hidden lg:inline-flex">
+                            <div className="hidden lg:inline-flex mb-5 spce-x-3 text-gray-800 whitespace-nowrap">
                                 <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Cancellation Flexibility</p>
 
                                 {/* Here I re-use the utility tailwind css classes in global.css: */}
